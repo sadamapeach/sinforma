a05ff10b34b936f3a61773e850ba2f1f471e5992
lailynovita <lailynovita29@gmail.com>
2024-01-01 12:19:42 +0700

    backend login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\MahasiswaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +15,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('login.index');
+// Route::get('/', function () {
+//     return view('login.index');
+// });
+
+
+Route::middleware('web')->group(function () {
+    Route::get('/login', [LoginController::class, 'index'])->middleware('guest')->name('login');
+    Route::post('/login', [LoginController::class, 'authenticate'])->name('authenticate');
+    Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth');
 });
+
+// Login
+// Route::get('/login', 'App\Http\Controllers\LoginController@index')->middleware('guest')->name('login');
+// Route::post('/login', 'App\Http\Controllers\LoginController@authenticate')->name('authenticate');
+// Route::post('/logout', 'App\Http\Controllers\LoginController@logout')->middleware('auth');
 
 /* Mahasiswa */
 Route::get('/dashboard_mahasiswa', 'App\Http\Controllers\MahasiswaController@index')->name('dashboard_mahasiswa');
 Route::get('/form_mahasiswa', 'App\Http\Controllers\MahasiswaController@form')->name('form_mahasiswa');
+
