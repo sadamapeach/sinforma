c1047905d2b3174d22c2bff9912be51d79b45d1b
lailynovita <lailynovita29@gmail.com>
2024-01-07 19:55:08 +0700

    middleware oke

--- routes/web.php
+++ routes/web.php
@@ -22,15 +22,15 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/form_mahasiswa', [MahasiswaController::class, 'form'])->name('form_mahasiswa');
 
     /* Admin */
-    Route::get('/dashboard_admin', [AdminController::class, 'index'])->name('dashboard_admin');
-    Route::get('/view_profil', [AdminController::class, 'viewProfile'])->name('view_profil');
-    Route::get('/edit_profil', [AdminController::class, 'viewEditProfile'])->name('edit_profil');
+    Route::get('/dashboard_admin', [AdminController::class, 'index'])->middleware('only_admin')->name('dashboard_admin');
+    Route::get('/view_profil', [AdminController::class, 'viewProfile'])->middleware('only_admin')->name('view_profil');
+    Route::get('/edit_profil', [AdminController::class, 'viewEditProfile'])->middleware('only_admin')->name('edit_profil');
     Route::post('/edit_profil', [AdminController::class, 'update'])->name('edit_profil');
     Route::get('/daftar_mhs', [AdminController::class, 'viewDaftarMhs'])->name('daftar_mhs');
     Route::get('/search_mhs', [AdminController::class, 'searchMahasiswa'])->name('search_mhs');
     Route::get('/filter_mhs', [AdminController::class, 'filterByStatus'])->name('filter_mhs');
     Route::get('/progress_mhs/{nim}', [AdminController::class, 'viewProgress'])->name('progress_mhs');
-    Route::get('/view_edit_status/{nim}', [AdminController::class, 'viewEditStatus'])->name('view_edit_status');
+    Route::get('/view_edit_status/{nim}', [AdminController::class, 'viewEditStatus'])->middleware('only_admin')->name('view_edit_status');
     Route::post('/delete_mhs/{nim}', [AdminController::class, 'delete2'])->name('delete_mhs');
     Route::post('/edit_mhs/{nim}', [AdminController::class, 'update2'])->name('edit_mhs');
 });
